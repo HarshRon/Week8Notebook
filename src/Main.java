368e2f00b4955fd0883a9bda39d6729d9c0fa757
10student <10student@nr.stu>
2023-10-16 09:37:26 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void Main(String[] args){
+        System.out.println("Today is Monday 10/16");
+        System.out.println("1 week until midterm is due");
+    }
+}
