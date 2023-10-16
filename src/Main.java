f931f7e70e2f81151e7bd38d71e80b0c323cdce0
10student <10student@nr.stu>
2023-10-16 09:40:00 -0400

    Updated notebook

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void Main(String[] args){
         System.out.println("Today is Monday 10/16");
         System.out.println("1 week until midterm is due");
+        System.out.println("due on Monday 10/23");
     }
 }
